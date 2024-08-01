1b3fb4a306d212e31f9aa40a4314842fd9b5b0df
Andrey Kuzmin <andrey.kuzmin.2806@mail.ru>
2024-08-01 13:30:34 +0300

    update dependencies and fix StCustomSelect

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 const BASE_URL = 'https://restcountries.com/v2/';
-export const ALL_COUNTRIES = BASE_URL + 'all?fields=name,capital,flags,population,region';
+export const ALL_COUNTRIES =
+  BASE_URL + 'all?fields=name,capital,flags,population,region';
 
-export const searchByCountry = (countryName?: string) => BASE_URL + '/name/' + countryName;
+export const searchByCountry = (countryName?: string) =>
+  BASE_URL + 'name/' + countryName;
 
-export const filterByCode = (codes?: string[]) => BASE_URL + 'alpha?codes=' + codes?.join(',');
+export const filterByCode = (codes?: string[]) =>
+  BASE_URL + 'alpha?codes=' + codes?.join(',');
